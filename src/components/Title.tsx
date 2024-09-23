b2fb29bf407aad974476ffc86a77cb1f2002e036
kimzeze <fridaynight@kakao.com>
2024-09-24 00:21:50 +0900

    [🗼markup] 제목 컴포넌트 마크업 추가

---
+++ src/components/Title.tsx
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <div className="border text-center text-white">
+      <h1 className="text-3xl font-bold">안성재 쉐프 말투 변환기</h1>
+      <p className="text-sm">안성재 쉐프의 말투로 변환하여 보여주는 웹사이트입니다.</p>
+    </div>
+  );
+}
