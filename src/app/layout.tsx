393567f6857a0029a89d06bf01142bd5bef138ce
kimzeze <fridaynight@kakao.com>
2024-09-24 00:23:33 +0900

    [💄style] 메인 스타일 임시 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="flex min-h-screen flex-col text-base antialiased">{children}</body>
     </html>
   );
 }
