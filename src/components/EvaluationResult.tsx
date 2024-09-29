62c6a1920312aee9b4edea81f03335648fb18d16
kimzeze <fridaynight@kakao.com>
2024-09-29 19:12:42 +0900

    [✨feat] 결과 컴포넌트 마크업 수정

--- src/components/EvaluationResult.tsx
+++ src/components/EvaluationResult.tsx
@@ -7,9 +7,9 @@ interface EvaluationResultProps {
 
 export function EvaluationResult({ food, evaluation }: EvaluationResultProps) {
   return (
-    <div className="rounded border-2 border-tertiary bg-primary p-4 text-white">
-      <h3 className="font-bold text-white">{food} 심사 결과:</h3>
-      <p>{evaluation}</p>
+    <div className="max-w-[600px] rounded-2xl border-2 border-tertiary bg-primary p-4 text-white">
+      {/* <h3 className="mb-2 font-bold text-white">{food} 심사 결과:</h3> */}
+      <p className="text-md font-medium">{evaluation}</p>
     </div>
   );
 }
