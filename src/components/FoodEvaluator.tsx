ed97e13855b251ed7a500bca261aa897367fbb55
kimzeze <fridaynight@kakao.com>
2024-09-29 19:12:54 +0900

    [✨feat] 모달 포탈을 사용해서 결과를 렌더링 하도록 수정

--- src/components/FoodEvaluator.tsx
+++ src/components/FoodEvaluator.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import React from 'react';
-import Image from 'next/image';
 import Button from '@/components/Button';
-import { useFoodEvaluator } from '@/lib/hooks/useFoodEvaluator';
 import { JudgeSelector } from './JudgeSelector';
-import { EvaluationResult } from './EvaluationResult';
+import useModal from '@/lib/hooks/useModal';
+import ResultModal from './ResultModal';
+import { useFoodEvaluator } from '@/lib/hooks/useFoodEvaluator';
+import { LoadingSpinner } from './LoadingSpinner';
 
 export default function FoodEvaluator() {
   const {
@@ -20,6 +21,20 @@ export default function FoodEvaluator() {
     handleRefresh,
   } = useFoodEvaluator();
 
+  const { openModal, closeModal, ModalPortal } = useModal();
+
+  const handleEvaluateAndOpenModal = async () => {
+    await handleEvaluate();
+    if (!error) {
+      openModal();
+    }
+  };
+
+  const handleCloseAndRefresh = () => {
+    closeModal();
+    handleRefresh();
+  };
+
   return (
     <div className="mx-auto flex w-full max-w-md flex-col space-y-4 p-4">
       <JudgeSelector selectedJudge={selectedJudge} onJudgeSelect={handleJudgeSelect} />
@@ -28,37 +43,25 @@ export default function FoodEvaluator() {
         value={food}
         onChange={handleInputChange}
         placeholder="평가받을 음식을 입력해주세요."
-        disabled={isLoading}
         className="w-full rounded-full border-2 border-gray-300 bg-primary px-10 py-2 text-center font-bold text-white"
       />
       <Button
         label="평가하기"
         type="button"
-        onClick={handleEvaluate}
+        onClick={handleEvaluateAndOpenModal}
         disabled={isLoading}
-        className="border-white bg-tertiary font-bold text-primary disabled:opacity-50"
+        className="border-white bg-tertiary font-bold text-primary"
       />
-      {/* <Button
-        label="새로고침"
-        type="button"
-        onClick={handleRefresh}
-        className="border-2 border-white bg-white text-primary hover:bg-blue-50"
-      /> */}
-      {isLoading && (
-        <div className="text-center">
-          <div className="mb-2">평가 중...</div>
-          <Image
-            src={selectedJudge === 'anSungJae' ? '/assets/ahn.png' : '/assets/baek.png'}
-            alt={`${selectedJudge === 'anSungJae' ? '안성재' : '백종원'} 이미지`}
-            width={600}
-            height={400}
-            layout="responsive"
-            priority
-          />
-        </div>
-      )}
       {error && <div className="rounded bg-red-100 p-2 text-center text-red-500">오류: {error}</div>}
-      {result && <EvaluationResult food={result.food} evaluation={result.evaluation} />}
+      {isLoading && <LoadingSpinner selectedJudge={selectedJudge} />}
+      <ModalPortal>
+        <ResultModal
+          closeModal={handleCloseAndRefresh}
+          result={result}
+          isLoading={isLoading}
+          selectedJudge={selectedJudge}
+        />
+      </ModalPortal>
     </div>
   );
 }
