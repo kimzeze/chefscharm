7538497aeffcef0cc1d7291e90c9f75e48480c16
kimzeze <fridaynight@kakao.com>
2024-10-02 18:53:02 +0900

    [💄style] 제목 마진 제거 및 패딩 x,y 수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
   return (
-    <div className="mt-5 p-4 text-center text-white">
+    <div className="px-4 py-6 text-center text-white">
       <h1 className="text-4xl font-bold">흑백 요리사 리뷰 작성기</h1>
     </div>
   );
