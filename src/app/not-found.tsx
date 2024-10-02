d30c819cbb43dfd15fb7d4fa297f2c18bc0ee0f5
kimzeze <fridaynight@kakao.com>
2024-10-03 02:04:16 +0900

    [🗼markup] not-found 페이지 마크업 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function NotFoundPage() {
   return (
-    <div className="mt-10 flex h-[300px] flex-col items-center justify-center text-center">
+    <div className="mt-10 flex h-[300px] flex-col items-center justify-center text-center text-white">
       <h2 className="mb-4 text-xl font-bold">죄송합니다 😢</h2>
-      <h2 className="mb-4 text-xl font-bold">지금은 사용할 수 없는 페이지입니다.</h2>
+      <h2 className="mb-4 text-xl font-bold">사용할 수 없는 페이지입니다.</h2>
       <Link className="text-lg font-semibold text-secondary transition-colors active:text-tertiary" href="/">
         메인 페이지로 돌아가기
       </Link>
