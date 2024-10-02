3627445fb98522ce9ff0d05d37bd5166b6a62c09
kimzeze <fridaynight@kakao.com>
2024-10-03 01:19:26 +0900

    [📦package] vercel anylitics 패키지 추가 및 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
+
 import './globals.css';
 
 export const metadata: Metadata = {
   title: '안성재 쉐프 말투 변환기',
   description: '안성재 쉐프의 말투로 변환하여 보여주는 웹사이트입니다.',
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,15 +14,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="flex min-h-screen flex-col bg-black text-base antialiased">
-        <div className="mx-auto w-full max-w-[480px]">
-          <main className="flex-grow">
+      <body className="flex min-h-screen flex-col overflow-x-hidden bg-black text-base antialiased">
+        <div className="mx-auto flex w-full max-w-[480px] flex-grow flex-col px-4">
+          <main className="flex flex-grow items-center justify-center">
             <h1 className="sr-only">흑백요리사 리뷰 작성기</h1>
             {children}
           </main>
           <div id="portal"></div>
         </div>
       </body>
+      <Analytics />
     </html>
   );
 }
